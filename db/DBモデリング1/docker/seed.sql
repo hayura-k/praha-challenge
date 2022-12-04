0c69eec7782f15554aac1e00f1fd779fb2894f07
yuki hara <hara.0509@icloud.com>
2022-12-05 01:44:53 +0900

    テーブル作成

--- db/DBモデリング1/docker/seed.sql
+++ db/DBモデリング1/docker/seed.sql
@@ -1,12 +1,69 @@
-CREATE DATABASE IF NOT EXISTS sample;
-USE sample;
+CREATE DATABASE IF NOT EXISTS sample_db;
+USE sample_db;
 
-CREATE TABLE IF NOT EXISTS sample_table
+-- テーブル作成部分
+CREATE TABLE IF NOT EXISTS customers
 (
-  `id`         int(11) NOT NULL AUTO_INCREMENT,
-  `sample`     text,
-  PRIMARY KEY (`id`)
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(255),
+  is_paid boolean,
+  phone_number varchar(15),
+  PRIMARY KEY (id)
 );
 
-INSERT INTO sample_table (sample) VALUES ("sample1");
-INSERT INTO sample_table (sample) VALUES ("sample2");
+CREATE TABLE IF NOT EXISTS food_prices
+(
+  id int NOT NULL AUTO_INCREMENT,
+  price int,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS set_food_categories
+(
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(255),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS foods
+(
+  id int NOT NULL AUTO_INCREMENT,
+  price_id int,
+  name varchar(255),
+  PRIMARY KEY (id),
+  foreign key fk_price_id (price_id) references food_prices(id)
+);
+
+CREATE TABLE IF NOT EXISTS set_foods
+(
+  id int NOT NULL AUTO_INCREMENT,
+  set_food_category_id int,
+  name varchar(255),
+  price int,
+  PRIMARY KEY (id),
+  foreign key fk_set_food_category_id (set_food_category_id) references set_food_categories(id)
+);
+
+CREATE TABLE IF NOT EXISTS orders
+(
+  id int NOT NULL AUTO_INCREMENT,
+  customer_id int,
+  total_price int,
+  total_amount int,
+  PRIMARY KEY (id),
+  foreign key fk_customer_id (customer_id) references customers(id)
+);
+
+CREATE TABLE IF NOT EXISTS order_details
+(
+  id int NOT NULL AUTO_INCREMENT,
+  order_id int,
+  food_id int,
+  set_food_id int,
+  has_wasabi boolean,
+  quantity int,
+  PRIMARY KEY (id),
+  foreign key fk_order_id (order_id) references orders(id),
+  foreign key fk_food_id (food_id) references foods(id),
+  foreign key fk_set_food_id (set_food_id) references set_foods(id)
+);
